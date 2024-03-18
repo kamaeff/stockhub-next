3bf9e73a1543e3dd7414f73a486f3f1fa7e3d94a
anton <kamaeff2@gmail.com>
2024-03-18 15:29:04 +0300

    fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,13 +3,13 @@ import {CircleUser, Home, Loader, PackageOpen} from 'lucide-react'
 import Link from 'next/link'
 import {AnimatePresence, motion} from 'framer-motion'
 import React, {useEffect, useState} from 'react'
+import Image from 'next/image'
 
 import Modal from './components/Basket'
 import {UseTg} from '../hooks/useTg'
 import Profile from './components/profile/Profile'
 
 import logo from '../../../public/logo.png'
-import Image from 'next/image'
 
 type SearchParamProps = {
   searchParams: Record<string, string> | null | undefined
@@ -28,24 +28,29 @@ export default function Header({searchParams}: SearchParamProps) {
   }
 
   const basket = searchParams?.basket
+  const profile = searchParams?.profile
 
   return (
     <div className='flex justify-between items-center mt-3 mx-2'>
       {/* TODO: Сделать проверку на реального юзера тг
         BUG: Пофиксить баг с неправильной загрузкой юзера */}
 
-      <button onClick={openModal} className='pl-2 flex items-center gap-1'>
-        <CircleUser strokeWidth={1} size={40} />
-      </button>
+      <Link
+        href='/?profile=true'
+        onClick={openModal}
+        className='pl-2 flex items-center gap-1'
+      >
+        <CircleUser strokeWidth={1} size={32} />
+      </Link>
 
-      <Image src={logo} priority={true} alt='logo' />
+      <Image priority={true} src={logo} alt='logo' />
 
       <Link
         href='/?basket=true'
         onClick={openModal}
         className='flex items-center pr-2'
       >
-        <PackageOpen size={40} strokeWidth={1} />
+        <PackageOpen size={32} strokeWidth={1} />
         <span className='italic text-xl pt-6'>2</span>
         {/* TODO: Выводит из бд колличество товаров в корзине */}
       </Link>
@@ -65,7 +70,7 @@ export default function Header({searchParams}: SearchParamProps) {
       </AnimatePresence>
 
       <AnimatePresence>
-        {isModalOpen && (
+        {profile && isModalOpen && (
           <motion.div
             initial={{opacity: 0, y: 1000}}
             animate={{opacity: 1, y: 0}}
