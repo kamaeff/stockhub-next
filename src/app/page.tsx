dfa4f6224caa6b64fabf21d029eb62b8a9eb0e10
anton <kamaeff2@gmail.com>
2024-03-18 12:44:50 +0300

    add animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Header from '@/components/header/Header'
 import Main from '@/components/main/Main'
+import Circles from './ui/Circles'
 interface SearchParamProps {
   searchParams: Record<string, string>
 }
 export default function Home(props: SearchParamProps) {
   return (
     <main>
+      <div className='background'>
+        <Circles />
+      </div>
       <Header searchParams={props.searchParams} />
       <Main searchParams={props.searchParams} />
     </main>
