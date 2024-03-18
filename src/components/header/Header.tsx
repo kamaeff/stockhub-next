6e5411cae90826784e291b4df8e9fc912f5e0653
anton <kamaeff2@gmail.com>
2024-03-18 14:00:52 +0300

    fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -30,7 +30,7 @@ export default function Header({searchParams}: SearchParamProps) {
   return (
     <div className='flex justify-between items-center mt-3 mx-2'>
       <div className=''>
-        {user && user.first_name ? (
+        {user ? (
           <Link
             href='/?profile=true'
             onClick={openModal}
